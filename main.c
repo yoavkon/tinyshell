44b8cb743c8cc16f509c9615a9508f7cd2cc75f5
Yoav <yoavk88@gmail.com>
2024-08-01 01:02:55 +0300

    environment variables

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
 #include <sys/wait.h>
 #include <errno.h>
 
@@ -14,13 +15,18 @@ void sigint_handler(int sig) { printf("\n"); }
 // get space seperated string into **parsed_str, returns number of args
 int parse_spaces_args(char *str, char **parsed_str);
 
+// get index of char in string
+int find_char_index(const char *str, const char c);
+            
+// replace word in a string
+char *replace_str(char *str, const char *substr_old, const char *substr_new);
+
 int main(void)
 {
     char input[MAX_INPUT_BUFFER]; // raw input
     char *argv[MAX_INPUT_BUFFER]; // space seperated input
     char cwd[MAX_INPUT_BUFFER];   // current working directory
     int argc;                     // amount of arguments
-    char c;                       // temporary char
     int pid, stat;                // process id, command exit code
 
     while (1)
@@ -35,6 +41,7 @@ int main(void)
         }
 
         // clear the stdin buffer, so that it does not wrap around
+        char c;
         if (strcspn(input, "\n") >= MAX_INPUT_BUFFER-1)
         {
             do {
@@ -44,7 +51,43 @@ int main(void)
 
         // remove newline
         input[strcspn(input, "\n")] = '\0';
+
+        // ignore empty command
         if (strlen(input) == 0) continue;
+
+        // handle environment variables
+        int env_name_index = find_char_index(input, '$');
+        while (env_name_index != -1) // iterate over every instance of '$'
+        {
+            // get the name of the variable
+            char *start = &input[env_name_index];
+            char *end = start+1;
+            while (isalnum(*(end+1)) || *(end+1) == '_') end++;
+            size_t length = end-start+1;
+
+            char *varname = malloc(sizeof(char)*(length+1));
+            if (varname == NULL)
+            {
+                perror("malloc error");
+                exit(1);
+            }
+            strncpy(varname, start, length);
+            varname[length] = '\0';
+
+            // get the value of the environment variable
+            char *value = getenv(varname+1); // varname without '$'
+            if (value == NULL) value = "\0";
+
+            // replace the variable name with its value
+            char *temp = replace_str(input, varname, value);
+            strncpy(input, temp, MAX_INPUT_BUFFER);
+            input[MAX_INPUT_BUFFER-1] = '\0';
+
+            free(varname);
+            free(temp);
+            // break; // TEMP
+            env_name_index = find_char_index(input, '$');
+        }
 
         // parse spaces
         argc = parse_spaces_args(input, argv);
@@ -116,4 +159,57 @@ int parse_spaces_args(char *str, char **parsed_str) {
         }
     }
     return count;
+}
+
+// find the index of a char in a string, return -1 if not found
+int find_char_index(const char *str, const char c)
+{
+    for (int i=0; i<strlen(str); i++)
+    {
+        if (str[i] == c)
+            return i;
+    }
+    return -1;
+}
+
+char *replace_str(char *str, const char *substr_old, const char *substr_new)
+{
+    int pos = 0;
+    int old_length = strlen(substr_old);
+    int new_length = strlen(substr_new);
+    int strlength = strlen(str);
+    int reslength = strlength - old_length + new_length;
+
+    char *res = malloc(sizeof(char)*(reslength + 1));
+
+    if (res == NULL) 
+    {
+        perror("malloc error");
+        exit(1);
+    }
+
+    // initialize
+    for (int i=0; i<reslength + 1; i++) res[i] = '\0';
+
+    // find position of substr_old
+    while (strstr(str + pos, substr_old) != NULL) pos++;
+    pos--;
+    
+    // copy last chars from str
+    int i=reslength, j=strlength;
+    while (i>pos+new_length && j>pos+old_length)
+    {
+        i--;
+        j--;
+        res[i] = str[j];
+    }
+
+    // copy variable value
+    for (int i=pos; i<pos+new_length; i++)
+        res[i] = substr_new[i-pos];
+
+    // copy first chars from str
+    for (int i=0; i<pos; i++) res[i] = str[i];
+
+    return res;
 }
